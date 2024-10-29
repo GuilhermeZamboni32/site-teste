60221bcc854e6278546b93482ec2a42b01de2a9f
GuilhermeZamboni32 <guilhermezamboni867@gmail.com>
2024-10-29 17:02:48 -0300

    site-teste

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './App.css';
+import TelaInicial from './Components/TelaInicial';
+
+function App() {
+ 
+
+  return (
+    <>
+     <TelaInicial />
+    </>
+  );
+}
+
+export default App;
